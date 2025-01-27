e78b9c89fd1a13bbce504afbfbefdf111a3f3e66
Samira <samirasanchez8090@gmail.com>
2025-01-27 10:38:21 -0500

    Button component modified to fit project design, button component imported in the contact component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,14 +3,17 @@ import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
-
+/*
+TODO:
+[]make the button longers so that it takes up the full width of the form
+[]change the text size of the button to be bigger and more prominent
+*/
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-stone-950 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 dark:focus-visible:ring-stone-300",
   {
     variants: {
       variant: {
-        default:
-          "bg-stone-900 text-stone-50 shadow hover:bg-stone-900/90 dark:bg-stone-50 dark:text-stone-900 dark:hover:bg-stone-50/90",
+        default: "text-white bg-pink-400 hover:bg-pink-300 focus:outline-none focus:ring-4 focus:ring-pink-300 font-medium rounded-md text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-pink-600 dark:hover:bg-pink-500 dark:focus:ring-pink-500 ",
         destructive:
           "bg-red-500 text-stone-50 shadow-sm hover:bg-red-500/90 dark:bg-red-900 dark:text-stone-50 dark:hover:bg-red-900/90",
         outline:
@@ -21,7 +24,7 @@ const buttonVariants = cva(
         link: "text-stone-900 underline-offset-4 hover:underline dark:text-stone-50",
       },
       size: {
-        default: "h-9 px-4 py-2",
+        default: "h-9 px-8 py-8",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
         icon: "h-9 w-9",
