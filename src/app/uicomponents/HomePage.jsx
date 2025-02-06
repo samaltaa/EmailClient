ab2b58c0f17aab3856634473f1aa67ab02cb6a2f
Samira <samirasanchez8090@gmail.com>
2025-02-06 09:28:25 -0500

    page folder set up, homeopage component defined

---
+++ src/app/uicomponents/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from 'next/link';
+import userRouter from 'next/router';
+
+function HomePage() {
+
+return(
+    <div>
+        
+    </div>
+)
+}
+
+export default HomePage
