fbcf9263581f28c4b8ae35e6007f2400541ec136
Samira <samirasanchez8090@gmail.com>
2025-02-10 14:30:49 -0500

    pink propagated loading icon added and styled

--- src/app/uicomponents/Loader.jsx
+++ src/app/uicomponents/Loader.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import PropagateLoader from "react-spinners/PropagateLoader";
+
+/*
+TODO: 
+[] make component take up full page width and height 
+[] bg should be a gradient color identical to the one on the home page
+[] center the loader icon and text in the center of screen
+[] make the loading message change if loading takes too long 
+*/
 
 function Loader() {
 
     return(
-        <div className="p-4">
+        <div className="w-[90%] flex-center h-10 p-4">
             <div className="flex justify-center items-center p-4">
-                <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900"></div>
+                <PropagateLoader color="#fa75ce"/>
             </div>
-            <div className="text-center text-gray-500">
-                Loading emails... This might take a few seconds.
+            <div className="text-center text-pink-500">
+                Loading Emails, please wait........
             </div>
         </div>
     )
