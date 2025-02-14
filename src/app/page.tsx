1ca7d7046a240e0f9315aa30d2f44b4f597f686b
Samira <samirasanchez8090@gmail.com>
2025-02-14 17:08:37 -0500

    emailcontent component created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@ import Contact from '@/app/pages/contact/page';
 import Inbox from './pages/inbox/page';
 import HomePage from './uicomponents/HomePage'
 import SideBar from './uicomponents/SideBar'
+import EmailContent from './uicomponents/EmailContent'
 
 
 export default function Home() {
   return (
     <main>
-      <SideBar/>
+      <EmailContent/>
       <HomePage/>
     </main>
   );
--- src/app/uicomponents/EmailContent.jsx
+++ src/app/uicomponents/EmailContent.jsx
@@ -3,8 +3,16 @@ import React from "react";
 function EmailContent() {
 
     return (
-        <div className="">
-
+        <div className="w-1/2 h-screen bg-pink-200 p-4 rounded-none shadow-lg">
+            <div className="border-b border-pink-400 pb-2">
+                <h2 className="text-xl font-semibold text-pink-800">this is where your email heading will go</h2>
+                <p className="text-sm text-pink-600">From: emailaddress@email.com</p>
+                <p className="text-sm text-pink-600">Date: 02/14/2024</p>
+            </div>
+            <div className="mt-4 p-4  w-full h-64 ">
+                <p className="text-pink-700"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id nibh dignissim, iaculis dolor sed, semper ante. </p>
+            </div>
         </div>
     )
 }
+export default EmailContent
